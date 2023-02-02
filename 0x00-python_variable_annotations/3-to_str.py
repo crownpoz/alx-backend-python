33360b18c5c29c3c2e1b39e73a81050a895d0640
crownpoz <agbeyoadebimpe@gmail.com>
2023-02-02 20:51:18 +0100

    annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''type-annotated function to_str'''
+
+import math
+
+
+def to_str(n: float) -> str:
+    '''function that returns string representation of float'''
+    return str(n)
